d422f274283e0a773aaaffebbeae96c19a7d8f01
Yukichka <yukiinsw@gmail.com>
2019-12-04 09:17:47 +0000

    Config added

--- src/components/services/firebase.js
+++ src/components/services/firebase.js
@@ -1,18 +1,10 @@
 import * as firebase from "firebase";
+import config from "./firebase-config";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAO7wsQBRFewVCuyFkGPQxQKH4AN-uug1g",
-  authDomain: "piclab-cbe78.firebaseapp.com",
-  databaseURL: "https://piclab-cbe78.firebaseio.com",
-  projectId: "piclab-cbe78",
-  storageBucket: "piclab-cbe78.appspot.com",
-  messagingSenderId: "822034909965",
-  appId: "1:822034909965:web:87eb6189ad12d7abefbe44",
-  measurementId: "G-GGJ6KBSFK4"
-};
+const firebaseConfig = config;
 
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-export default database ;
+export default database;

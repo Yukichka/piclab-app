e09ab135cdd74322bd98798a1fae7eaf96b8c170
Yukichka <yukiinsw@gmail.com>
2019-12-04 09:19:46 +0000

    Config temp added

---
+++ src/components/services/firebase-config-tmp.js
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "XXXXX",
+  authDomain: "XXXX.firebaseapp.com",
+  databaseURL: "https://XXXXX.firebaseio.com",
+  projectId: "XXXXX",
+  storageBucket: "XXXX.appspot.com",
+  messagingSenderId: "10525XXXXXXXXX98824487",
+  appId: "1:1052598824487:web:20eaac906XXXXXXXXX701e0b6802",
+  measurementId: "G-PXXXXXXS"
+};
